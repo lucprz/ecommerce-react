866ccd040430755e4518b0b0c85dc152619bb1f1
lucprz <lucprz.gm@gmail.com>
2021-11-05 17:59:00 -0300

    Whole app migrated to React

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PasswordRecovery from "../pages/PasswordRecovery";
+import Checkout from "../pages/Checkout";
+import CreateAccount from "../pages/CreateAccount";
+import MyAccount from "../pages/MyAccount";
+import NewPassword from "../pages/NewPassword";
+import Orders from "../pages/Orders";
+import SendEmail from "../pages/SendEmail";
 import NotFound from "../pages/NotFound";
 import Layout from "../containers/Layout";
-import Login from "../containers/Login";
-import RecoveryPassword from "../containers/RecoveryPassword";
 import "../styles/global.css";
 
 const App = () => {
@@ -13,12 +19,18 @@ const App = () => {
       <Layout>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<CreateAccount />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/send-email" element={<SendEmail />} />
           <Route
             exact
-            path="/recovery-password"
-            element={<RecoveryPassword />}
+            path="/password-recovery"
+            element={<PasswordRecovery />}
           />
+          <Route exact path="/my-account" element={<MyAccount />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
