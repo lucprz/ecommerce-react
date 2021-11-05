713db7369b178bf9a2533272fd4751d418936327
lucprz <lucprz.gm@gmail.com>
2021-11-05 12:13:51 -0300

    css and sass added. Webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,6 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
   },
@@ -27,6 +29,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
@@ -34,5 +40,13 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
   ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3005,
+  },
 };
