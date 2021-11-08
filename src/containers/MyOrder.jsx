9e7893921cbf5fb57764dd7621dd8f6bfa13687b
lucprz <lucprz.gm@gmail.com>
2021-11-08 12:59:49 -0300

    Insertion of product into the cart and sum logic added

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import OrderItem from "../components/OrderItem";
-import "../styles/MyOrder.scss";
+import React, { useContext } from "react";
+import AppContext from "../context/AppContext";
+import OrderItem from "@components/OrderItem";
+import "@styles/MyOrder.scss";
+import arrowLeft from "@icons/flechita.svg";
 
 const MyOrder = () => {
+  const { state } = useContext(AppContext);
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <aside className="MyOrder">
       <div className="title-container">
-        <img src="./icons/flechita.svg" alt="arrow" />
+        <img src={arrowLeft} alt="arrow" />
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-        <OrderItem />
+        {state.cart.map((item) => (
+          <OrderItem product={item} key={`orderItem-${item.id}`} />
+        ))}
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
